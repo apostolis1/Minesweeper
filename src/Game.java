5d86821e6660baff2495f21c9c00dd89dcbdc0c8
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2022-12-26 17:29:31 +0200

    Implement basic functionality in Internal representations

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,20 @@
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Game {
     int gridSize, numberOfMoves, numberOfMines;
     boolean hasSuperMine;
-    int [][] grid;
+    TileInternal [][] grid;
 
     public Game(int gridSize, int numberOfMines, boolean hasSuperMine) {
         this.gridSize = gridSize;
         this.numberOfMoves = 0;
         this.numberOfMines = numberOfMines;
         this.hasSuperMine = hasSuperMine;
+        this.grid = new TileInternal[this.gridSize][this.gridSize];
 
         ArrayList<Integer> allIndices = new ArrayList<>();
         for (int i =0 ; i< this.gridSize; i++) {
@@ -28,20 +32,72 @@ public class Game {
         // Due to shuffling, the indices are random
         for (int i=0; i<numberOfMines; ++i)
             mineIndices.add(allIndices.get(i));
+        int superMinePosition = -1 ;
+        if (this.hasSuperMine) {
+            superMinePosition = mineIndices.get(0);
+        }
+        for (int i =0; i< this.gridSize; ++i) {
+            for (int j = 0; j < this.gridSize; ++j) {
+                boolean hasMine = mineIndices.contains(i*this.gridSize + j);
+                boolean isSuperMine = (superMinePosition == i*this.gridSize + j);
+                TileInternal tile = new TileInternal(i, j, hasMine, isSuperMine);
+                grid[i][j] = tile;
+            }
+        }
+    }
+
+    void tileLeftClicked(int x, int y) {
 
     }
 
-    void tileClicked(int x, int y) {
-
+    void tileRightClicked(int x, int y) {
+        grid[x][y].rightClicked();
     }
 
-    boolean isTileMine() {
-
+    boolean isTileMine(int x, int y) {
+        return this.grid[x][y].getMine();
     }
 
-    boolean isTileSuperMine() {
-
+    boolean isTileSuperMine(int x, int y) {
+        return this.grid[x][y].getSuperMine();
     }
 
-    void
+    public ArrayList<TileInternal> getNeighborsByCoordinates(int x, int y) {
+        ArrayList<TileInternal> result = new ArrayList<>();
+        TileInternal temp;
+        // Get elements of above row
+        temp = getTileByCoordinates(x-1,y-1);
+        if (temp != null)
+            result.add(temp);
+        temp = getTileByCoordinates(x-1,y);
+        if (temp != null)
+            result.add(temp);
+        temp = getTileByCoordinates(x-1,y+1);
+        if (temp != null)
+            result.add(temp);
+        // Get elements of same row
+        temp = getTileByCoordinates(x,y-1);
+        if (temp != null)
+            result.add(temp);
+        temp = getTileByCoordinates(x,y+1);
+        if (temp != null)
+            result.add(temp);
+        // Get elements of next row
+        temp = getTileByCoordinates(x+1,y-1);
+        if (temp != null)
+            result.add(temp);
+        temp = getTileByCoordinates(x+1,y);
+        if (temp != null)
+            result.add(temp);
+        temp = getTileByCoordinates(x+1,y+1);
+        if (temp != null)
+            result.add(temp);
+        return result;
+    }
+
+    public TileInternal getTileByCoordinates(int x, int y) {
+        if (x < 0 | x >= this.gridSize | y < 0 | y>= this.gridSize)
+            return null;
+        return this.grid[x][y];
+    }
 }
--- src/TileInternal.java
+++ src/TileInternal.java
@@ -1,2 +1,40 @@
 public class TileInternal {
+    // This is the internal representation for the Tiles
+    // It is used to separate the backend logic from the frontend
+
+    public int x,y;
+    private Boolean isMine, isSuperMine, flagSet, isRevealed;
+
+    public TileInternal(int x, int y, Boolean isMine, Boolean isSuperMine) {
+        this.x = x;
+        this.y = y;
+        this.isMine = isMine;
+        this.isSuperMine = isSuperMine;
+        this.flagSet = false;
+        this.isRevealed = false;
+    }
+
+    public Boolean getRevealed() {
+        return isRevealed;
+    }
+
+    public void setRevealed(Boolean revealed) {
+        isRevealed = revealed;
+    }
+
+    public Boolean getMine() {
+        return isMine;
+    }
+
+    public void setMine(Boolean mine) {
+        isMine = mine;
+    }
+
+    public Boolean getSuperMine() {
+        return isSuperMine;
+    }
+
+    public void rightClicked() {
+        this.flagSet = !this.flagSet;
+    }
 }
