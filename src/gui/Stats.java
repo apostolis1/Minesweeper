fe0040e91533c8be29da143d8fc533dfad268b46
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2023-02-26 03:28:30 +0200

    Create Stats class to keep track of individual game stats

---
+++ src/gui/Stats.java
@@ -0,0 +1,56 @@
+package gui;
+
+public class Stats {
+    public int getMines() {
+        return mines;
+    }
+
+    public void setMines(int mines) {
+        this.mines = mines;
+    }
+
+    public int getTries() {
+        return tries;
+    }
+
+    public void setTries(int tries) {
+        this.tries = tries;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public boolean isPlayerWon() {
+        return playerWon;
+    }
+
+    public void setPlayerWon(boolean playerWon) {
+        this.playerWon = playerWon;
+    }
+
+    int mines, tries, time;
+    boolean playerWon;
+
+    public Stats(int mines, int tries, int time, boolean playerWon) {
+        this.mines = mines;
+        this.tries = tries;
+        this.time = time;
+        this.playerWon = playerWon;
+    }
+
+    private String getPlayerWon() {
+        if (playerWon)
+            return "Player";
+        return "CPU";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d %d %s", mines, tries, time, getPlayerWon());
+    }
+}
